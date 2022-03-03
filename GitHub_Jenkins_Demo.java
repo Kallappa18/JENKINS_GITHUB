8113f130b3b39b9d3e2d3e161f5eb176f6ddf777
Kallappa18 <97930904+Kallappa18@users.noreply.github.com>
2022-03-03 17:41:06 +0530

    Update GitHub_Jenkins_Demo.java

--- GitHub_Jenkins_Demo.java
+++ GitHub_Jenkins_Demo.java
@@ -2,5 +2,6 @@ public class GitHub_Jenkins_Demo {
     public static void main(String[] args) {
         System.out.println("taking input from github displaying result in jenkins");
         int d=45;
+        int h=40;
     }
 }
